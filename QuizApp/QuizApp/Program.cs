f7f191b95bb86428cbd77f8f8dc88dee3afc27cd
USMCsky <57950108+USMCsky@users.noreply.github.com>
2026-06-11 21:25:58 -0500

    Refactored QuizApp

--- QuizApp/QuizApp/Program.cs
+++ QuizApp/QuizApp/Program.cs
@@ -1,86 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Text;
+using System.Linq;
 
-string question1 = "What is the capital of Germany";
-string answer1 = "Berlin";
+StartQuiz();
 
-string question2 = "What is 2+2";
-string answer2 = "4";
-
-string question3 = "What color do you get by mixing blue and yellow";
-string answer3 = "Green";
-
-int score = 0;
-
-Console.WriteLine(question1);
-string userAnswer1 = Console.ReadLine();
-if (Normalize(userAnswer1) == Normalize(answer1))
+static void StartQuiz()
 {
-    Console.WriteLine("Correct!");
-    score = score + 1;
-}
-else
-{
-    Console.WriteLine("Wrong, the correct answer is: " + answer1);
-}
+    var questions = new (string Question, string Answer)[]
+    {
+        ("What is the capital of Germany", "Berlin"),
+        ("What is 2+2", "4"),
+        ("What color do you get by mixing blue and yellow", "Green"),
+        ("What is the capital of Texas", "Austin")
+    };
 
-Console.WriteLine(question2);
-string userAnswer2 = Console.ReadLine();
-if (Normalize(userAnswer2) == Normalize(answer2))
-{
-    Console.WriteLine("Correct!");
-    score = score + 1;
-}
-else
-{
-    Console.WriteLine("Wrong, the correct answer is: " + answer2);
-}
+    int score = 0;
 
+    foreach (var (question, answer) in questions)
+    {
+        Console.WriteLine(question);
+        var userAnswer = Console.ReadLine();
+        if (Normalize(userAnswer) == Normalize(answer))
+        {
+            Console.WriteLine("Correct!");
+            score++;
+        }
+        else
+        {
+            Console.WriteLine($"Wrong, the correct answer is: {answer}");
+        }
+    }
 
-Console.WriteLine(question3);
-string userAnswer3 = Console.ReadLine();
-if (Normalize(userAnswer3) == Normalize(answer3))
-{
-    Console.WriteLine("Correct!");
-    score = score + 1;
-}
-else
-{
-    Console.WriteLine("Wrong, the correct answer is: " + answer3);
-}
+    Console.WriteLine($"Quiz completed! Your final score is: {score}/{questions.Length}");
+    if (score == questions.Length)
+    {
+        Console.WriteLine("Excellent! You got all the answers right!");
+    }
+    else if (score > 0)
+    {
+        Console.WriteLine("Good Job, but keep learning!");
+    }
+    else
+    {
+        Console.WriteLine("Try again and see if you can get some answers right next time!");
+    }
 
-Console.WriteLine($"Quiz completed! Your final score is: {score}/3");
-if (score == 3)
-{
-    Console.WriteLine("Excellent! You got all the answers right!");
+    Console.ReadKey();
 }
-else if (score > 0)
-{
-    Console.WriteLine("Good Job, but keep learning!");
-}
-else
-{
-    Console.WriteLine("Try again and see if you can get some answers right next time!");
-}
-
-
-Console.ReadKey();
 
 static string Normalize(string s)
 {
     if (string.IsNullOrWhiteSpace(s)) return string.Empty;
 
-    var sb = new StringBuilder();
-    foreach (var c in s)
-    {
-        if (char.IsLetterOrDigit(c) || char.IsWhiteSpace(c))
-        {
-            sb.Append(char.ToLowerInvariant(c));
-        }
-        // skip punctuation and other characters
-    }
-
-    var parts = sb.ToString().Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-    return string.Join(" ", parts).Trim();
+    var cleaned = new string(s.Where(c => char.IsLetterOrDigit(c) || char.IsWhiteSpace(c)).ToArray()).ToLowerInvariant();
+    var parts = cleaned.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+    return string.Join(" ", parts);
 }
